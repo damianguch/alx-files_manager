04e5a74c82ecb3f41d3a6c6ec68a771a1fc0df5a
damian oguche <damianguch@yahoo.com>
2024-02-11 20:54:01 +0100

    utils/redis.js file modified

--- utils/redis.js
+++ utils/redis.js
@@ -7,14 +7,11 @@ import { promisify } from 'util';
 class RedisClient {
   constructor () {
     this.client = redis.createClient();
+
     this.getAsync = promisify(this.client.get).bind(this.client);
 
     this.client.on('error', (error) => {
       console.log(`Redis client not connected to the server: ${error.message}`);
-    });
-
-    this.client.on('connect', () => {
-      //   console.log('Redis client connected to the server');
     });
   }
 
